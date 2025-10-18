9f9ee8840e939b69846e2b90fa12279b4f339eca
NGUYEN-MINH-TRUC <nmtruc.study@gmail.com>
2025-10-18 08:48:52 +0700

    Update Database

--- docs/Database/data/admin account.sql
+++ docs/Database/data/admin account.sql
@@ -5,8 +5,3 @@ GO
 
 INSERT INTO [dbo].[users] ([username], [email], [password_hash], [full_name], [bio], [role]) VALUES
 ('superadmin', 'admin@pronaflow.io', '$2a$11$7mi5ilhKCwuHqyeAWL1JIu2.EkAhWgKZEAsPahI1LoxrbPYjFzAOu', 'PronaFlow Admin', N'Module sprint cross-platform next-generation interface analyzing designing secure.', 'admin')
-
-SELECT * FROM users;
-SELECT * FROM workspaces;
-DELETE FROM workspaces;
-DELETE FROM users;
